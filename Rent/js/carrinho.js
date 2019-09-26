85f7a77cfec4fa87b8a731471f161b5e3c3ada54
Mateus Arthur <mateusarthur00@hotmail.com>
2019-09-26 10:26:38 -0300

    att

--- Rent/js/carrinho.js
+++ Rent/js/carrinho.js
@@ -9,7 +9,7 @@ function mudar_two(){
 	document.getElementById("valor_prod2").innerHTML = str;
 };
 function mudar_three(){
-	var x = document.getElementById("qnt_prod2").value;
+	var x = document.getElementById("qnt_prod3").value;
 	var str = "R$" + 20*x + ",00";
-	document.getElementById("valor_prod2").innerHTML = str;
+	document.getElementById("valor_prod3").innerHTML = str;
 };

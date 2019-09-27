5a5692d85b7ce8f367233237ad49786804de75af
Aluno <aluno@uffs.gov.br>
2019-09-27 14:16:54 -0300

    att

--- Rent/js/carrinho.js
+++ Rent/js/carrinho.js
@@ -11,19 +11,19 @@ document.getElementById("r_frete").onclick = function(){
 
 function mudar_one(){
 	var x = document.getElementById("qnt_prod1").value;
-	var str = "R$" + 20*x + ",00";
+	var str = "R$" + 20*x + ",00(R$20,00)";
 	document.getElementById("valor_prod1").innerHTML = str;
 	calcular_stotal();
 };
 function mudar_two(){
 	var x = document.getElementById("qnt_prod2").value;
-	var str = "R$" + 20*x + ",00";
+	var str = "R$" + 20*x + ",00(R$20,00)";
 	document.getElementById("valor_prod2").innerHTML = str;
 	calcular_stotal();
 };
 function mudar_three(){
 	var x = document.getElementById("qnt_prod3").value;
-	var str = "R$" + 20*x + ",00";
+	var str = "R$" + 20*x + ",00(R$20,00)";
 	document.getElementById("valor_prod3").innerHTML = str;
 	calcular_stotal();
 };
